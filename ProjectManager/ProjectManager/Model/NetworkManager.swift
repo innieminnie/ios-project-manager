a7234395580cfd968a4efed75b11ad51543eb2d1
GREEN <humains29@gmail.com>
2021-03-24 14:02:58 +0900

    [#20] feat: NetworkManager 싱글턴 클래스 생성

---
+++ ProjectManager/ProjectManager/Model/NetworkManager.swift
@@ -0,0 +1,24 @@
+import Foundation
+import Alamofire
+
+class NetworkManager {
+    static let shared = NetworkManager()
+    
+    func fetch() {
+        
+    }
+    
+    func create() {
+        
+    }
+    
+    func update() {
+        
+    }
+    
+    func delete() {
+        
+    }
+}
+
+let networkManager = NetworkManager.shared
